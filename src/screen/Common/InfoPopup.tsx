3bdbe92f23e505f5d3fc25605db221b6edf575ca
Kelyan Pegeot Selme <kelyan.pegeot-selme@eviden.com>
2026-04-27 20:59:14 +0200

    fix: negative charging limit

---
+++ src/screen/Common/InfoPopup.tsx
@@ -0,0 +1,38 @@
+import { View } from "react-native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+type InfoPopupProps = {
+    backgroundColour: string;
+    icon: string;
+    iconColour: string;
+    children: React.JSX.Element | React.JSX.Element[];
+
+}
+
+function InfoPopup({ backgroundColour, icon, iconColour, children }: InfoPopupProps): React.JSX.Element {
+    return (
+        <View
+            style={{
+                backgroundColor: backgroundColour,
+                padding: 10,
+                borderRadius: 10
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 10,
+                    width: '100%'
+                }}
+            >
+                <Icon name={icon} size={20} color={iconColour} />
+                <View style={{ flex: 1 }}>
+                    {children}
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default InfoPopup;
