9aad8d238978c95d273ef2d5f3a8a50dc0fe331f
Kelyan Pegeot Selme <kelyan.pegeot-selme@eviden.com>
2026-06-02 19:37:37 +0200

    chore: remove apple watch version

---
+++ ios/KelecWatchOs Watch App/DeadView.swift
@@ -0,0 +1,18 @@
+//
+//  DeadView.swift
+//  KelecWatchOs Watch App
+//
+//  Created by Kelyan PEGEOT SELME on 02/06/2026.
+//
+
+import SwiftUI
+
+struct DeadView: View {
+    var body: some View {
+        Text("Renault killed Kelec on Apple Watch")
+    }
+}
+
+#Preview {
+    DeadView()
+}
