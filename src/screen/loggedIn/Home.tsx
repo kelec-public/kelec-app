279ba0ee3ada39f41acbf10e20a6c0166eb7b928
Kelyan Pegeot Selme <kelyan.pegeot-selme@eviden.com>
2026-06-14 18:47:40 +0200

    fix: icons for android

--- src/screen/loggedIn/Home.tsx
+++ src/screen/loggedIn/Home.tsx
@@ -1,5 +1,5 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeBottomTabNavigator } from '@bottom-tabs/react-navigation';
 
@@ -7,12 +7,38 @@ import CarsView from './CarsTab/CarsView';
 import ProfileView from './ProfileTab/ProfileView';
 import SettingsView from './SettingsTab/SettingsView';
 import MainContext from '../../lib/Contexts/MainContext';
+import { Platform, useColorScheme } from 'react-native';
 
 const Tab = createNativeBottomTabNavigator();
 
 function Home(): React.JSX.Element {
 
     const { languageHandler } = useContext(MainContext);
+
+    const isDarkMode = useColorScheme() === 'dark';
+    const [androidIcons, setAnroidIcons] = useState<{
+        car?: any;
+        person?: any;
+        settings?: any;
+    }>({});
+
+    useEffect(() => {
+        if (Platform.OS !== 'android') return;
+        const MaterialIcons = require('react-native-vector-icons/MaterialIcons').default;
+        const color = isDarkMode ? '#fff' : '#000';
+        Promise.all([
+            MaterialIcons.getImageSource('directions-car', 20, color),
+            MaterialIcons.getImageSource('person', 20, color),
+            MaterialIcons.getImageSource('settings', 20, color),
+        ]).then(([carIcon, personIcon, settingsIcon]) => {
+            setAnroidIcons({
+                car: carIcon,
+                person: personIcon,
+                settings: settingsIcon,
+            });
+        });
+    }, [isDarkMode]);
+
 
     return (
         <NavigationContainer>
@@ -22,7 +48,9 @@ function Home(): React.JSX.Element {
                     component={CarsView}
                     options={{
                         title: languageHandler.getTranslation('cars'),
-                        tabBarIcon: () => ({ sfSymbol: 'car.fill', androidIcon: 'directions_car' }),
+                        tabBarIcon: () => Platform.OS === 'android'
+                            ? androidIcons.car
+                            : { sfSymbol: 'car.fill' },
                     }}
                 />
                 <Tab.Screen
@@ -30,7 +58,9 @@ function Home(): React.JSX.Element {
                     component={ProfileView}
                     options={{
                         title: languageHandler.getTranslation('account'),
-                        tabBarIcon: () => ({ sfSymbol: 'person.fill', androidIcon: 'person' }),
+                        tabBarIcon: () => Platform.OS === 'android'
+                            ? androidIcons.person
+                            : { sfSymbol: 'person.fill' },
                     }}
                 />
                 <Tab.Screen
@@ -38,7 +68,9 @@ function Home(): React.JSX.Element {
                     component={SettingsView}
                     options={{
                         title: languageHandler.getTranslation('settings'),
-                        tabBarIcon: () => ({ sfSymbol: 'gearshape.fill', androidIcon: 'settings' }),
+                        tabBarIcon: () => Platform.OS === 'android'
+                            ? androidIcons.settings
+                            : { sfSymbol: 'gearshape.fill' },
                     }}
                 />
             </Tab.Navigator>
